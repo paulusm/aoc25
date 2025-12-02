d3a16132895a8efa145e553d2e37aa7c640ec8d8
Paul Matthews <paulusm@gmail.com>
2025-12-02 17:20:05 +0000

    init

---
+++ 1.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+data <- read_lines(file="data/1-test.txt")
+pos <- 50
+
+data |> map(\(x) {
+  instruction <- x |> str_match("([L|R])(\\d*)")
+  amount <- as.integer(instruction[1,3])
+  pos <<- ifelse(instruction[1,2] == "R", (pos + amount) %% 100, (pos - amount) %% 100)
+  return(pos)
+})
