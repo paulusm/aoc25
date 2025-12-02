6d666e17f31f22edf5edc179f1a420a56a2a8e82
Paul Matthews <paulusm@gmail.com>
2025-12-02 17:30:41 +0000

    ud

--- 1.R
+++ 1.R
@@ -7,5 +7,5 @@ data |> map(\(x) {
   instruction <- x |> str_match("([L|R])(\\d*)")
   amount <- as.integer(instruction[1,3])
   pos <<- ifelse(instruction[1,2] == "R", (pos + amount) %% 100, (pos - amount) %% 100)
-  return(pos)
-})
+  pos
+}) |> reduce(\(x,y) y <- ifelse(y == 0, x + 1, x))
