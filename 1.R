81fd069cac2bf92028a4f8af7d08fb36cabc16b6
Paul Matthews <paulusm@gmail.com>
2025-12-02 21:30:39 +0000

    nfw

--- 1.R
+++ 1.R
@@ -10,7 +10,7 @@ pt1 <- data |> map(\(x) {
   instruction <- x |> str_match("([L|R])(\\d*)")
   amount <- as.integer(instruction[1,3])
   pos <<- ifelse(instruction[1,2] == "R", (pos + amount) %% 100, (pos - amount) %% 100)
-  zeropasses <<- zeropasses +  ifelse(instruction[1,2] == "R", as.integer((pos + amount) / 100), pos > 0 & pos - amount < 0)
+  zeropasses <<- zeropasses +  ifelse(instruction[1,2] == "R", as.integer((pos + amount) / 100), (pos > 0) & (pos - amount < 0 ))
 
   pos
 }) |> reduce(\(x,y)  ifelse(y == 0, x + 1, x),.init = 0)
