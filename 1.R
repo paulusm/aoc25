607ff16c96e15c8c174e69323f18af325a501e0b
Paul Matthews <paulusm@gmail.com>
2025-12-02 21:12:37 +0000

    bah

--- 1.R
+++ 1.R
@@ -1,11 +1,19 @@
-library(tidyverse)
+library(purrr)
+library(readr)
+library(stringr)
 
 data <- read_lines(file="data/1-test.txt")
 pos <- 50
+zeropasses <- 0
 
-data |> map(\(x) {
+pt1 <- data |> map(\(x) {
   instruction <- x |> str_match("([L|R])(\\d*)")
   amount <- as.integer(instruction[1,3])
   pos <<- ifelse(instruction[1,2] == "R", (pos + amount) %% 100, (pos - amount) %% 100)
+  zeropasses <<- zeropasses +  ifelse(instruction[1,2] == "R", as.integer((pos + amount) / 100), pos > 0 & pos - amount < 0)
+
   pos
-}) |> reduce(\(x,y) y <- ifelse(y == 0, x + 1, x))
+}) |> reduce(\(x,y)  ifelse(y == 0, x + 1, x),.init = 0)
+
+print(pt1)
+print(pt1 + zeropasses)
