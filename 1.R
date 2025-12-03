07c190791734fa76e72d1b8adee34602968fc992
Paul Matthews <paulusm@gmail.com>
2025-12-03 16:07:09 +0000

    oh dear

--- 1.R
+++ 1.R
@@ -2,18 +2,21 @@ library(purrr)
 library(readr)
 library(stringr)
 
-data <- read_lines(file="data/1-test.txt")
+data <- read_lines(file="data/1.txt")
 pos <- 50
 zeropasses <- 0
 
 pt1 <- data |> map(\(x) {
   instruction <- x |> str_match("([L|R])(\\d*)")
   amount <- as.integer(instruction[1,3])
+  cycles <- as.integer(amount / 100)
+  remainder <- amount - cycles * 100
+  zeropasses <<- zeropasses + cycles + ifelse(instruction[1,2] == "R", (pos + remainder) > 100, (pos!=0) & (pos - remainder < 0) ) 
   pos <<- ifelse(instruction[1,2] == "R", (pos + amount) %% 100, (pos - amount) %% 100)
-  zeropasses <<- zeropasses +  ifelse(instruction[1,2] == "R", as.integer((pos + amount) / 100), (pos > 0) & (pos - amount < 0 ))
-
+ 
   pos
-}) |> reduce(\(x,y)  ifelse(y == 0, x + 1, x),.init = 0)
+  }) |> reduce(\(x,y)  ifelse(y == 0, x + 1, x),.init = 0)
 
-print(pt1)
-print(pt1 + zeropasses)
+
+print(paste("Part One:", pt1))
+print(paste("Part Two:", pt1 + zeropasses))
