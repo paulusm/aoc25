04bd09e62c1295b299e9e20a684876d734e4d271
Paul Matthews <paulusm@gmail.com>
2025-12-05 21:05:18 +0000

    5b no dice

--- 02.R
+++ 02.R
@@ -3,6 +3,30 @@ library(purrr)
 result <- 0
 
 input <- read_lines(file = "data/2.txt") |> str_split(",")
+
+# input[[1]] |>
+#   as.list() |>
+#   map(\(x) {
+#     lims <- str_split(x, "-")
+#     seq(as.numeric(lims[[1]][1]), as.numeric(lims[[1]][2])) |>
+#       map(\(y) {
+#         codeaschar <- as.character(y)
+#         thelen <- nchar(codeaschar)
+#         if (thelen %% 2 == 0) {
+#           if (
+#             substring(codeaschar, 1, thelen / 2) ==
+#               substring(codeaschar, thelen / 2 + 1, thelen)
+#           ) {
+#             result <<- result + y
+#           }
+#         }
+#       })
+#   })
+
+# print(paste("Part1 ", result))
+
+result <- 0
+
 input[[1]] |>
   as.list() |>
   map(\(x) {
@@ -11,16 +35,28 @@ input[[1]] |>
       map(\(y) {
         codeaschar <- as.character(y)
         thelen <- nchar(codeaschar)
-        if (thelen %% 2 == 0) {
-          if (
-            substring(codeaschar, 1, thelen / 2) ==
-              substring(codeaschar, thelen / 2 + 1, thelen)
-          ) {
-            result <<- result + y
-          }
-        }
+        # 2, 3, 4 and 5 length repeats
+        match <- F
+        seq(1:(thelen / 2)) |>
+          map(\(a) {
+            if (!match) {
+              seq(2:(thelen - a + 2)) |>
+                map(\(b) {
+                  candidate <- rep(substring(codeaschar, 1, a), b) |>
+                    str_flatten()
+                  if (candidate == codeaschar) {
+                    #print(paste("gotcha", codeaschar, candidate))
+                    result <<- result + y
+                    match <<- T
+                  }
+                })
+            }
+          })
       })
   })
 
+# 36855614794 too low
+# 36862281460 too high
 
-print(paste("Part1 ", result))
+#test 4174379265
+print(paste("Part2 ", result))
---
+++ 05.R
@@ -0,0 +1,74 @@
+library(readr)
+library(data.table)
+library(purrr)
+library(dtplyr)
+library(dplyr)
+library(stringr)
+options(scipen = 999)
+
+data <- read_lines(file = "data/5.txt")
+
+dtFresh <- data.table(from = numeric(), to = numeric())
+dtSamples <- data.table(sample = numeric())
+
+data |>
+  map(\(x) {
+    range <- str_split_1(x, "-")
+    if (length(range) == 2) {
+      dtFresh <<- rbind(dtFresh, list(as.double(range[1]), as.double(range[2])))
+    } else {
+      if (nchar(x) > 0) {
+        dtSamples <<- rbind(dtSamples, list(as.double(x)))
+      }
+    }
+  })
+
+# dtSamples$sample |>
+#   map(\(x) {
+#     ifelse(nrow(dtFresh |> filter(x >= from & x <= to)) > 0, 1, 0)
+#   }) |>
+#   reduce(sum) |>
+#   print()
+
+dtFreshClean <- data.table(from = numeric(), to = numeric())
+dtFreshNonOverlap <- dtFresh
+
+dtFresh$from |>
+  map2(dtFresh$to, \(a, b) {
+    dtFresh$from |>
+      map2(dtFresh$to, \(x, y) {
+        if (a != x & b != y) {
+          # Overlapping head
+          if (a <= x & b > x & b < y) {
+            dtFreshClean <<- dtFreshClean |> rbind(list(a, y))
+            dtFreshNonOverlap <<- dtFreshNonOverlap[
+              -((from == a & to == b) | (from == x & to == y)),
+            ]
+          }
+          # Overlapping tail
+          if (a > x & a < y & b >= y) {
+            dtFreshClean <<- dtFreshClean |> rbind(list(x, b))
+            dtFreshNonOverlap <<- dtFreshNonOverlap[
+              -((from == a & to == b) | (from == x & to == y)),
+            ]
+          }
+          # Enclosed
+          if (a > x & b < y) {
+            dtFreshNonOverlap <<- dtFreshNonOverlap[
+              -(from == a & to == b),
+            ]
+          }
+        }
+      })
+  })
+
+dtFreshClean <- dtFreshClean[-duplicated(dtFreshClean), ]
+dtFreshNonOverlap <- dtFreshNonOverlap[-duplicated(dtFreshNonOverlap), ]
+
+# 214452980313054 too low
+# 455191274181386, 554540882371704 too high
+dtFreshClean |>
+  bind_rows(dtFreshNonOverlap) |>
+  mutate(diff = to - from) |>
+  summarise(sum(diff)) |>
+  print()
