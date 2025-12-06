9cefc1b3b70c0ce72d314df211a6207d9eff11b9
Paul Matthews <paulusm@gmail.com>
2025-12-06 14:56:49 +0000

    6 pt2

--- 06.R
+++ 06.R
@@ -2,6 +2,7 @@ library(readr)
 library(purrr)
 library(stringr)
 
+# Part One
 data <- read_lines(file = "data/6.txt") |>
   map(\(x) x |> str_split_1(" ")) |>
   map(\(x) x |> discard(\(z) z == "")) |>
@@ -19,3 +20,46 @@ data <- read_lines(file = "data/6.txt") |>
   summarise(grandtot = sum(tot))
 
 print(data$grandtot)
+
+
+#Part 2, not proud of this :()
+data <- read_lines(file = "data/6.txt")
+symbolpos <- data[[5]] |> str_locate_all("[\\*|\\+]")
+symbolpos <- symbolpos[[1]][, 1]
+
+rejigged <- data |>
+  map(\(y) {
+    symbolpos |>
+      map(\(x) {
+        if (x > 1) {
+          y |> str_sub(start = x - 1, end = x - 1) <<- "|"
+        }
+      })
+    y |> str_split_1("\\|")
+  }) |>
+  as_tibble(.name_repair = "unique")
+
+stringops <- function(theRow, theOp) {
+  runtot <- ""
+  for (j in 1:nchar(theRow[1])) {
+    theNo <- ""
+    for (i in 1:ncol(theRow)) {
+      theNo <- paste0(theNo, str_trim((str_sub(theRow[i], j, j))))
+    }
+    runtot <- paste0(runtot, theNo, str_trim(theOp))
+  }
+  runtot <- str_sub(runtot, 1, nchar(runtot) - 1)
+  return(eval(parse(text = runtot)))
+}
+
+
+gttib <- rejigged |>
+  rename("operation" = last_col()) |>
+  rowwise() |>
+  mutate(
+    tot = stringops(across(!last_col()), operation)
+  ) |>
+  ungroup() |>
+  summarise(grandtot = sum(tot))
+
+print(gttib$grandtot)
