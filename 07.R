fd5381450c5bcf893a14e8cb0eb700244b0ec3ed
Paul Matthews <paulusm@gmail.com>
2025-12-07 23:19:40 +0000

    7b

--- 07.R
+++ 07.R
@@ -5,14 +5,16 @@ library(stringr)
 data <- read_lines(file = "data/7-test.txt")
 splits <- 0
 beampos <- list(str_locate(data[[1]], "S")[1, 1])
-hist <- list()
+
+
+## Part One
 
 data <- data |>
   discard_at(1) |>
-  map(\(line) {
+  imap(\(line, i) {
     newline <- line
     beampos <<- beampos |>
-      imap(\(y, i) {
+      map(\(y) {
         if (substring(line, y, y) == ".") {
           substring(newline, y) <<- "|"
           y
@@ -22,7 +24,6 @@ data <- data |>
               substring(newline, y + 1) <<- "|"
               substring(newline, y - 1) <<- "|"
               splits <<- splits + 1
-              hist[[length(hist) + 1]] <<- list(i, y - 1, y + 1)
               list(y - 1, y + 1)
             }
           }
@@ -35,3 +36,11 @@ data <- data |>
   })
 
 print(splits)
+
+## Part Two
+forks <- data <- data |>
+  discard_at(1) |>
+  imap(\(line, i) {
+    forks <- (line |> str_locate_all("\\^"))[[1]][, 1]
+    print(forks)
+  })
