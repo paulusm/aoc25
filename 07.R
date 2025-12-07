ece292d20ab8d2bfd8a6bb8059822195a1a3a877
Paul Matthews <paul2.matthews@uwe.ac.uk>
2025-12-07 19:03:21 +0000

    capture history

--- 07.R
+++ 07.R
@@ -2,16 +2,17 @@ library(readr)
 library(purrr)
 library(stringr)
 
-data <- read_lines(file = "data/7.txt")
+data <- read_lines(file = "data/7-test.txt")
 splits <- 0
 beampos <- list(str_locate(data[[1]], "S")[1, 1])
-print(beampos)
+hist <- list()
+
 data <- data |>
   discard_at(1) |>
   map(\(line) {
     newline <- line
     beampos <<- beampos |>
-      map(\(y) {
+      imap(\(y, i) {
         if (substring(line, y, y) == ".") {
           substring(newline, y) <<- "|"
           y
@@ -21,6 +22,7 @@ data <- data |>
               substring(newline, y + 1) <<- "|"
               substring(newline, y - 1) <<- "|"
               splits <<- splits + 1
+              hist[[length(hist) + 1]] <<- list(i, y - 1, y + 1)
               list(y - 1, y + 1)
             }
           }
