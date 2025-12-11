338716b487546a7e4b2447771a3ebddc8f7dc245
Paul Matthews <paul2.matthews@uwe.ac.uk>
2025-12-11 09:47:36 +0000

    11 a

--- 11.R
+++ 11.R
@@ -1,15 +1,32 @@
 library(readr)
 library(purrr)
 library(stringr)
+library(dplyr)
+library(igraph)
 
-input <- read_lines(file = "data/11-test.txt")
+input <- read_lines(file = "data/11.txt")
 
-input |>
+netdf <- tibble()
+net <- input |>
   map(\(x) {
     parts <- x |> str_split_1(":")
     parts[2] |>
       map(\(y) {
-        outputs <- y |> str_trim() |> str_split(" ")
-        c(parts[1], outputs)
+        outputs <- y |>
+          str_trim() |>
+          str_split_1(" ") |>
+          map(\(z) {
+            list(parts[1], z)
+          })
       })
+  }) |>
+  flatten() |>
+  flatten() |>
+  map(\(x) {
+    netdf <<- rbind(netdf, x)
   })
+names(netdf) <- c("from", "to")
+
+netobj <- graph_from_data_frame(netdf)
+paths <- netobj |> k_shortest_paths("you", "out", k = 1000)
+print(paths$epaths |> length())
