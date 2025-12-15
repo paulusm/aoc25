c9b65896ad218b779b3ebf128c7843630b433a5e
Paul Matthews <paul2.matthews@uwe.ac.uk>
2025-12-15 18:09:42 +0000

    ON the 5b case

--- 05.R
+++ 05.R
@@ -23,48 +23,25 @@ data |>
     }
   })
 
-# dtSamples$sample |>
-#   map(\(x) {
-#     ifelse(nrow(dtFresh |> filter(x >= from & x <= to)) > 0, 1, 0)
-#   }) |>
-#   reduce(sum) |>
-#   print()
+dtSamples$sample |>
+  map(\(x) {
+    ifelse(nrow(dtFresh |> filter(x >= from & x <= to)) > 0, 1, 0)
+  }) |>
+  reduce(sum) |>
+  print()
 
 # Part 2 not yet working
-rm(dtFreshClean)
-dtFreshClean <- data.table(from = numeric(), to = numeric())
-dtFreshNonOverlap <- dtFresh
-
-dtFreshNonOverlap$from |>
-  map2(dtFreshNonOverlap$to, \(a, b) {
-    dtFreshNonOverlap$from |>
-      map2(dtFreshNonOverlap$to, \(x, y) {
-        if (a != x & b != y) {
-          # Overlapping head
-          if (a < x & b > x & b < y) {
-            dtFreshClean <<- dtFreshClean |> rbind(list(a, y))
-            dtFreshNonOverlap <<- dtFreshNonOverlap[
-              -((from == a & to == b) | (from == x & to == y)),
-            ]
-          }
-          # Overlapping tail
-          if (a > x & a < y & b > y) {
-            dtFreshClean <<- dtFreshClean |> rbind(list(x, b))
-            dtFreshNonOverlap <<- dtFreshNonOverlap[
-              -((from == a & to == b) | (from == x & to == y)),
-            ]
-          }
-          # Enclosed
-          if (a > x & b < y) {
-            dtFreshNonOverlap <<- dtFreshNonOverlap[
-              -(from == a & to == b),
-            ]
-          }
-        }
-      })
+dtNoOverlaps <- data.table(newfrom = numeric(), newto = numeric())
+dtFresh |>
+  pmap(\(from, to) {
+    overlaps <- dtNoOverlaps[
+      (from > newfrom & to > newto & from < newto) |
+        (from < newfrom & to < newto & to < newfrom),
+    ]
+    print(paste("overlaps", nrow(overlaps)))
+    if (nrow(overlaps) == 0) {
+      dtNoOverlaps <<- dtNoOverlaps |> rbind(list(from, to))
+    } else {
+      print(paste("overlaps:", nrow(overlaps)))
+    }
   })
-
-
-dtOverall <- dtFreshClean |> bind_rows(dtFreshNonOverlap)
-dtOverall <- unique(dtOverall)
-dtOverall |> mutate(diff = (to - from) + 1) |> summarise(sum(diff)) |> print()
