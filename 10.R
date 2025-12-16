c1f86de876ef89b1befb4d5af2db0122830dbc17
Paul Matthews <paulusm@gmail.com>
2025-12-16 11:30:18 +0000

    10

--- 10.R
+++ 10.R
@@ -18,42 +18,46 @@ data <- input |>
         pluck(1) |>
         map(\(y) {
           match <- str_match(y, "\\(([0-9|,]+)\\)")
-          match[2] |> unlist() |> str_split_1(",") |> as.integer() |> map_vec(~ .x + 1)
+          match[2] |>
+            unlist() |>
+            str_split_1(",") |>
+            as.integer() |>
+            map_vec(~ .x + 1)
         })
     )
   })
 
-<<<<<<< HEAD
-data |> map(\(x) {})
-=======
-pressButtons <- function(states, target, buttons,its){
+pressButtons <- function(states, target, buttons, its) {
   #print(paste(paste(target, collapse=",") , "iteration", its))
   match <- F
-  states <- states |> map(\(state){
-    candidate <- state
-    #print(candidate)
-    buttons |> map(\(button){
-      candidate[button] <- !candidate[button]
-      if(identical(candidate,target)){
-        match <<-T
-      }
-      candidate
-    }) 
-}) |> flatten()
-  if(match){
+  states <- states |>
+    map(\(state) {
+      candidate <- state
+      #print(candidate)
+      buttons |>
+        map(\(button) {
+          candidate[button] <- !candidate[button]
+          if (identical(candidate, target)) {
+            match <<- T
+          }
+          candidate
+        })
+    }) |>
+    flatten()
+  if (match) {
     return(its)
-  } else{
+  } else {
     #print(paste("states length", length(states)))
     return(pressButtons(states, target, buttons, its + 1))
   }
-
 }
 
-data |> map(\(x){
-  its <- 1
-  pattern <- x |> pluck(1)
-  base <- rep(F,length(pattern))
-  pressButtons(list(base), pattern, x |> pluck(3), 1)
-}) |> reduce(sum) |> print()
-
->>>>>>> 262d414d28bdd32a88ee36ed8503dde91fc0b69a
+data |>
+  map(\(x) {
+    its <- 1
+    pattern <- x |> pluck(1)
+    base <- rep(F, length(pattern))
+    pressButtons(list(base), pattern, x |> pluck(3), 1)
+  }) |>
+  reduce(sum) |>
+  print()
